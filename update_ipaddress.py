770d772583de245e2c173187b004daa5e6e1f068
Mask-eng <34024451+Mask-eng@users.noreply.github.com>
2021-02-20 23:24:39 +0900

    On branch feature  Changes to be committed:  コード整形途中
    	modified:   update_ipaddress.py

--- update_ipaddress.py
+++ update_ipaddress.py
@@ -29,17 +29,17 @@ import json
 # password
 import credential
 
-# Variable chara
-MYDNS_URL      =   'https://www.mydns.jp/login.html'
+# Variable character
+MYDNS_URL           =   'https://www.mydns.jp/login.html'
 WEBHOOK_URL         =    credential.WEBHOOK_URL
 
 
 #post_to_mydns.py
 print(credential.MYDNS_USER, credential.MYDNS_PASSWORD)
 
-#------------------------------
+#--------------------------------------------------
 # post to mydns service
-#------------------------------
+#--------------------------------------------------
 def post_to_mydns(username, password):
     response_from_mydns = requests.get(
             MYDNS_URL, auth=HTTPBasicAuth( username, password )
@@ -47,12 +47,9 @@ def post_to_mydns(username, password):
     return response_from_mydns
 ###end
 
-result_from_mydns = post_to_mydns(credential.MYDNS_USER, credential.MYDNS_PASSWORD)
-
-
-#------------------------------
+#--------------------------------------------------
 # post to slack
-#------------------------------
+#--------------------------------------------------
 def post_to_slack(payload_message):
     print(payload_message)
     response_from_slack = requests.post(
@@ -62,7 +59,7 @@ def post_to_slack(payload_message):
             )
 
 ## exac
-#result_from_mydns = post_to_mydns(credential.MYDNS_USER, credential.MYDNS_PASSWORD)
+result_from_mydns = post_to_mydns(credential.MYDNS_USER, credential.MYDNS_PASSWORD)
 ###failed message example
 # @channnel
 # 
@@ -71,35 +68,49 @@ def post_to_slack(payload_message):
 # 
 #####
 
+#--------------------------------------------------
+# check http post response
+#--------------------------------------------------
+if result_from_mydns.status_code == 200:
+    # post succeed message
+    post_to_slack('Login and IP address notification succeeded.')
 
-succeed_message='Login and IP address notification succeeded.'
-failed_message=(
+else:
+    # post failed message
+    post_to_slack(
             '<!channel>\r' + 
             'Request to mydns returned an error ' +  
             '`' + str(result_from_mydns.status_code) + '`'
             ', the response is:\r' + 
             '```' + result_from_mydns.text + '```'
-        )
-#print(failed_message)
+            )
 
-#if result_from_mydns.status_code == 200:
+#-------------------------------------------------------------------------
+#succeed_message='Login and IP address notification succeeded.'
+#failed_message=(
+#            '<!channel>\r' + 
+#            'Request to mydns returned an error ' +  
+#            '`' + str(result_from_mydns.status_code) + '`'
+#            ', the response is:\r' + 
+#            '```' + result_from_mydns.text + '```'
+#        )
+#
+#if result_from_mydns.status_code != 200:
 #    #post_to_slack('Login and IP address notification succeeded.')
 #    post_to_slack(succeed_message)
 #
 #else:
-#    #post_to_slack(result_from_mydns.text)
 #    post_to_slack(failed_message)
-
-def check_http_response(response):
-    if response != 200:
-        # post success message.
-        message = succeed_message
-        return message
-    else:
-        # post failed message and log.
-        message = failed_message
-        return message 
-print('hoge')
-print(result_from_mydns.status_code)
-print(check_http_response(result_from_mydns.status_code))
-post_to_slack(check_http_response(result_from_mydns.status_code))
+#def check_http_response(response):
+#    if response != 200:
+#        # post success message.
+#        message = succeed_message
+#        return message
+#    else:
+#        # post failed message and log.
+#        message = failed_message
+#        return message 
+#print('hoge')
+#print(result_from_mydns.status_code)
+#print(check_http_response(result_from_mydns.status_code))
+#post_to_slack(check_http_response(result_from_mydns.status_code))
